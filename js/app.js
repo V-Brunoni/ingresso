f0e095ecda5769a52b4b159523b49e704f16550f
Vinicius Brunoni <viniciusbrunoni8@gmail.com>
2024-10-09 22:35:09 -0300

    Adicionando função comprar

---
+++ js/app.js
@@ -0,0 +1,69 @@
+function comprar(){
+    let tipoIngresso = document.getElementById('tipo-ingresso');
+    let quantidade = parseInt(document.getElementById('qtd').value);
+
+    let quantidadePista = parseInt(document.getElementById('qtd-pista').textContent);
+    let quantidadeSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    let quantidadeInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+
+    switch (tipoIngresso.value) {
+        case 'pista':
+            if(quantidade > quantidadePista){
+                alert('Quantidade informada acima do disponível!');
+            } else {
+                quantidadePista = quantidadePista - quantidade;
+                document.getElementById('qtd-pista').textContent = quantidadePista;
+                alert('Compra realizada com sucesso!');
+            }
+            break;
+        case 'superior':
+            if(quantidade > quantidadeSuperior){
+                alert('Quantidade informada acima do disponível!');
+            } else {
+                quantidadeSuperior = quantidadeSuperior - quantidade;
+                document.getElementById('qtd-superior').textContent = quantidadeSuperior;
+                alert('Compra realizada com sucesso!');
+            }
+            break;
+        default:
+            if(quantidade > quantidadeInferior){
+                alert('Quantidade informada acima do disponível!');
+            } else {
+                quantidadeInferior = quantidadeInferior - quantidade;
+                document.getElementById('qtd-inferior').textContent = quantidadeInferior;
+                alert('Compra realizada com sucesso!');
+            }
+    }
+}
+
+//    if (tipoIngresso.value == 'pista') {
+//         if(quantidade > quantidadePista){
+//             alert('Quantidade informada acima do disponível!');
+//         } else {
+//             quantidadePista = quantidadePista - quantidade;
+//             document.getElementById('qtd-pista').textContent = quantidadePista;
+//             alert('Compra realizada com sucesso!');
+            
+//         }
+
+//    } else if (tipoIngresso.value == 'superior') {
+//         if(quantidade > quantidadeSuperior){
+//             alert('Quantidade informada acima do disponível!');
+//         } else {
+//             quantidadeSuperior = quantidadeSuperior - quantidade;
+//             document.getElementById('qtd-superior').textContent = quantidadeSuperior;
+//              alert('Compra realizada com sucesso!');
+        
+//         }
+
+//     } else {
+//         if(quantidade > quantidadeInferior){
+//             alert('Quantidade informada acima do disponível!');
+//         } else {
+//             quantidadeInferior = quantidadeInferior - quantidade;
+//             document.getElementById('qtd-inferior').textContent = quantidadeInferior;
+//              alert('Compra realizada com sucesso!');
+        
+//         }
+
+//     }
